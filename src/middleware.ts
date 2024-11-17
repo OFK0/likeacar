f4f4e383742e89a01fc6886eaca1b356c28edbe0
Ömer Faruk Küçük <omerkucuk@teknasyon.com>
2024-11-17 22:56:22 +0300

    unstable dynamic fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,6 @@ import { routing } from './i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-  // Match only internationalized pathnames
+  unstable_allowDynamic: ['**/node_modules/lodash/**/*.js'],
   matcher: ['/', '/(de|en)/:path*'],
 };
